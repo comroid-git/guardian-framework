fb070f3c8d7906a0d5e13f93b2752bd267fe81b2
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-11-26 14:50:05 +0100

    Update JacksonXMLAdapter.java

--- src/uniform/main/java8compat/org/comroid/uniform/adapter/xml/jackson/JacksonXMLAdapter.java
+++ src/uniform/main/java8compat/org/comroid/uniform/adapter/xml/jackson/JacksonXMLAdapter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.comroid.uniform.adapter.model.JacksonAdapter;
 
 public final class JacksonXMLAdapter extends JacksonAdapter {
+    public static final JacksonXMLAdapter instance = new JacksonXMLAdapter();
+
     private JacksonXMLAdapter() {
         super("application/xml", new XmlMapper());
     }
