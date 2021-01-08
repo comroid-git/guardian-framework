2e211a112e157967268321f64c329795b1b0b0c7
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-01-08 23:57:28 +0100

    Update KeyedPipe.java

--- src/mutatio/main/java/org/comroid/mutatio/pipe/impl/KeyedPipe.java
+++ src/mutatio/main/java/org/comroid/mutatio/pipe/impl/KeyedPipe.java
@@ -1,27 +1,20 @@
 package org.comroid.mutatio.pipe.impl;
 
 import org.comroid.api.Polyfill;
-import org.comroid.api.Rewrapper;
 import org.comroid.mutatio.pipe.BiPipe;
 import org.comroid.mutatio.pipe.BiStageAdapter;
 import org.comroid.mutatio.pipe.Pipe;
 import org.comroid.mutatio.ref.KeyedReference;
-import org.comroid.mutatio.ref.Reference;
-import org.comroid.mutatio.ref.ReferenceIndex;
-import org.comroid.mutatio.ref.ReferenceMap;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // todo this class is still shit
 public class KeyedPipe<InK, InV, K, V> extends BasicPipe<InV, V> implements BiPipe<K, V> {
-    private final Map<K, KeyedReference<K, V>> accessors = new ConcurrentHashMap<>();
+    private final Map<Integer, KeyedReference<K, V>> accessors = new ConcurrentHashMap<>();
+    private final BiStageAdapter<InK, InV, K, V> adapter;
     private final boolean isSameKeyType;
-    private final EntryIndex entryIndex;
 
     public KeyedPipe(
             Pipe<InV> old,
@@ -30,7 +23,7 @@ public class KeyedPipe<InK, InV, K, V> extends BasicPipe<InV, V> implements BiPi
     ) {
         super(old, adapter, autoEmptyLimit);
 
-        this.entryIndex = new EntryIndex();
+        this.adapter = adapter;
 
         KeyedReference<InK, InV> test;
         this.isSameKeyType = adapter instanceof BiStageAdapter.Support.Filter;/*
@@ -51,46 +44,8 @@ public class KeyedPipe<InK, InV, K, V> extends BasicPipe<InV, V> implements BiPi
 
     @Override
     public Stream<KeyedReference<K, V>> streamRefs() {
-        return super.streamRefs().map(Polyfill::uncheckedCast);
-    }
-
-    private final class EntryIndex implements ReferenceIndex<KeyedReference<K, V>> {
-        private final Map<Integer, Reference<KeyedReference<K, V>>> indexAccessors = new ConcurrentHashMap<>();
-
-        @Override
-        public List<KeyedReference<K, V>> unwrap() {
-            return Collections.unmodifiableList(stream().collect(Collectors.toList()));
-        }
-
-        @Override
-        public int size() {
-            return KeyedPipe.this.size();
-        }
-
-        @Override
-        public boolean add(KeyedReference<K, V> item) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(KeyedReference<K, V> item) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-            KeyedPipe.this.clear();
-        }
-
-        @Override
-        public Stream<? extends Reference<KeyedReference<K, V>>> streamRefs() {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Reference<KeyedReference<K, V>> getReference(int index) {
-            return indexAccessors.computeIfAbsent(index, k -> Reference
-                    .provided(() -> KeyedPipe.this.getReference(index)));
-        }
+        // generate accessors
+        refs.generateAccessors(accessors, adapter, BiStageAdapter::advance);
+        return accessors.values().stream();
     }
 }
