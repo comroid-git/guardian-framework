db900da2b21b2089dcfcd745321c45a7c25e7fa6
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-01-10 11:57:01 +0100

    possible implementation of ValueAdapter

--- src/uniform/main/java/org/comroid/uniform/model/ValueAdapter.java
+++ src/uniform/main/java/org/comroid/uniform/model/ValueAdapter.java
@@ -1,7 +1,20 @@
 package org.comroid.uniform.model;
 
-import org.comroid.uniform.node.UniNode;
+import org.comroid.api.HeldType;
+import org.comroid.uniform.ValueType;
 
-public interface ValueAdapter {
-    UniNode.Type
+public abstract class ValueAdapter<B> {
+    protected final B base;
+    protected final ValueType<B> actualType;
+
+    protected ValueAdapter(B base, ValueType<B> actualType) {
+        this.base = base;
+        this.actualType = actualType;
+    }
+
+    public final <T> T asType(HeldType<T> type) {
+        return actualType.convert(asActualType(), type);
+    }
+
+    protected abstract B asActualType();
 }
