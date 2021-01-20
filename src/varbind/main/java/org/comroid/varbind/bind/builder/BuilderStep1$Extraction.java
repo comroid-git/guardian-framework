a15ad5accaa8271fe26a9285e04ac4dc8c26fb89
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-01-20 23:19:13 +0100

    Update BuilderStep1$Extraction.java

--- src/varbind/main/java/org/comroid/varbind/bind/builder/BuilderStep1$Extraction.java
+++ src/varbind/main/java/org/comroid/varbind/bind/builder/BuilderStep1$Extraction.java
@@ -3,9 +3,11 @@ package org.comroid.varbind.bind.builder;
 import org.comroid.api.Polyfill;
 import org.comroid.api.ValueType;
 import org.comroid.uniform.node.UniObjectNode;
+import org.comroid.util.StandardValueType;
 import org.comroid.varbind.bind.GroupBind;
 import org.comroid.varbind.bind.VarBind;
 import org.comroid.varbind.container.DataContainer;
+import org.intellij.lang.annotations.MagicConstant;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
@@ -21,7 +23,8 @@ public final class BuilderStep1$Extraction<SELF extends DataContainer<? super SE
         return extractAsArray(null);
     }
 
-    public <E extends Serializable> BuilderStep2$Remapping<SELF, E> extractAs(ValueType<E> valueType) {
+    public <E extends Serializable> BuilderStep2$Remapping<SELF, E> extractAs(
+            @MagicConstant(valuesFromClass = StandardValueType.class) ValueType<E> valueType) {
         return new BuilderStep2$Remapping<>(group, fieldName, valueType, VarBind.ExtractionMethod.VALUE);
     }
 
