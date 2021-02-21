be2fd99acb77165c69618990d3d63b01781dea50
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-02-21 19:04:59 +0100

    Update OS.java

--- src/core/main/java/org/comroid/common/os/OS.java
+++ src/core/main/java/org/comroid/common/os/OS.java
@@ -1,9 +1,16 @@
 package org.comroid.common.os;
 
+import org.comroid.api.IntEnum;
 import org.comroid.api.Named;
+import org.comroid.api.Polyfill;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public enum OS implements Named {
     WINDOWS("win"),
@@ -12,8 +19,10 @@ public enum OS implements Named {
     SOLARIS("sunos");
 
     public static final OS current = detect();
+    private static final Pattern ArchPattern = Pattern.compile(".*(?<num>\\d{2,3}).*");
+    public static final Architecture currentArchitecture = detectArchitecture();
 
-    private final String[] validators;
+    private final List<String> validators;
 
     @Override
     public String getName() {
@@ -22,18 +31,42 @@ public enum OS implements Named {
     }
 
     OS(String... validators) {
-        this.validators = validators;
+        this.validators = Collections.unmodifiableList(Arrays.asList(validators));
     }
 
-    static OS detect() {
+    private static OS detect() {
         if (current != null)
             return current;
 
+        String osName = System.getProperty("os.name");
         for (OS value : values()) {
-            if (Arrays.asList(value.validators).contains(System.getProperty("os.name").toLowerCase()))
+            final String osLow = osName.toLowerCase();
+            if (value.validators.stream().anyMatch(osLow::contains))
                 return value;
         }
 
-        throw new NoSuchElementException("Unknown OS");
+        throw new NoSuchElementException("Unknown OS: " + osName);
+    }
+
+    private static Architecture detectArchitecture() {
+        final String arch = System.getProperty("os.arch");
+        try {
+            Matcher matcher = ArchPattern.matcher(arch);
+            String num = Polyfill.regexGroupOrDefault(matcher, "num", null);
+            if (num == null)
+                throw new IllegalStateException("No numeric identifier found in: " + arch);
+            return Architecture.valueOf('x' + num);
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid Architecture: " + arch, e);
+        }
+    }
+
+    public enum Architecture implements IntEnum {
+        x32, x64, x86;
+
+        @Override
+        public @NotNull Integer getValue() {
+            return Integer.parseInt(name().substring(1));
+        }
     }
 }
