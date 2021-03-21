8c67b44a8d804e33d57df3cafc6ad008dbda7f5f
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-03-21 14:36:09 +0100

    Update UniObjectNode.java

--- src/uniform/main/java/org/comroid/uniform/node/UniObjectNode.java
+++ src/uniform/main/java/org/comroid/uniform/node/UniObjectNode.java
@@ -1,9 +1,12 @@
 package org.comroid.uniform.node;
 
+import org.comroid.annotations.inheritance.MustExtend;
 import org.comroid.uniform.model.NodeType;
+import org.comroid.uniform.node.impl.UniObjectNodeImpl;
 
 import java.util.Map;
 
+@MustExtend(UniObjectNodeImpl.class)
 public interface UniObjectNode extends Map<String, Object>, UniNode {
     @Override
     default NodeType getNodeType() {
