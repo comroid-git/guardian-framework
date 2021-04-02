d584645d10c5c3d1f177de15e57ff2ac5715b50c
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-02 22:42:36 +0200

    implement StringSerializable interface

--- src/uniform/main/java/org/comroid/uniform/model/Serializable.java
+++ src/uniform/main/java/org/comroid/uniform/model/Serializable.java
@@ -1,10 +1,12 @@
 package org.comroid.uniform.model;
 
+import org.comroid.api.StringSerializable;
 import org.comroid.uniform.node.UniNode;
 
-public interface Serializable {
+public interface Serializable extends StringSerializable {
     UniNode toUniNode();
 
+    @Override
     default String toSerializedString() {
         return toUniNode().toString();
     }
