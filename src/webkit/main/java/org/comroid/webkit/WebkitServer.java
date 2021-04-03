000df9540a6b6d880d48d02a09305073dcf2a58d
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-03 03:16:34 +0200

    Update WebkitServer.java

--- src/webkit/main/java/org/comroid/webkit/WebkitServer.java
+++ src/webkit/main/java/org/comroid/webkit/WebkitServer.java
@@ -1,4 +1,42 @@
 package org.comroid.webkit;
 
-public final class WebkitServer {
+import org.comroid.api.ContextualProvider;
+import org.comroid.restless.server.RestServer;
+import org.comroid.restless.server.ServerEndpoint;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.concurrent.ScheduledExecutorService;
+
+public final class WebkitServer implements ContextualProvider.Underlying {
+    private final ContextualProvider context;
+    private final ScheduledExecutorService executor;
+    private final ServerEndpoint[] additionalEndpoints;
+    private final RestServer rest;
+
+    @Override
+    public ContextualProvider getUnderlyingContextualProvider() {
+        return context;
+    }
+
+    public WebkitServer(
+            ContextualProvider context,
+            ScheduledExecutorService executor,
+            String urlBase,
+            InetAddress inetAddress,
+            int port,
+            ServerEndpoint... additionalEndpoints
+    ) throws IOException {
+        this.context = context.plus("Webkit Server", this);
+        this.executor = executor;
+        this.additionalEndpoints = additionalEndpoints;
+        WebkitEndpoints endpoints = new WebkitEndpoints();
+        this.rest = new RestServer(this.context, executor, urlBase, inetAddress, port, endpoints.getEndpoints());
+    }
+
+    private final class WebkitEndpoints {
+        public ServerEndpoint[] getEndpoints() {
+            return null;
+        }
+    }
 }
