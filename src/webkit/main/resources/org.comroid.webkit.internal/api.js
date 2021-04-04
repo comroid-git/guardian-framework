1ce6e420a99954c27ff8166782f332e058a70bd5
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-05 00:37:34 +0200

    Update api.js

--- src/webkit/main/resources/org.comroid.webkit.internal/api.js
+++ src/webkit/main/resources/org.comroid.webkit.internal/api.js
@@ -3,8 +3,19 @@ if (isDebug === undefined)
 
 function initAPI() {
     console.debug('loading socket')
-    var ws = new WebSocket(isDebug ? "ws://localhost:42001" : `ws://${window.location.host}/websocket`);
 
-    ws.onopen = () => console.debug('opened')
-    ws.onmessage = (msg) => console.debug('data received: ', msg)
+    function handleMessage(data) {
+        if (data.indexOf(`id:`) !== 0) {
+            console.debug('Invalid data received', data);
+            return;
+        }
+
+        let idEnd = data.indexOf(';');
+        let id = data.substr(3, idEnd - 3);
+
+        document.getElementById(id).innerHTML = data.substr(idEnd + 1)
+    }
+
+    var ws = new WebSocket(isDebug ? "ws://localhost:42001" : `ws://${window.location.host}/websocket`);
+    ws.onmessage = (msg) => handleMessage(msg.data);
 }
