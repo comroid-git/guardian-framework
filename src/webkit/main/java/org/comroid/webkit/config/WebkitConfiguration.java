d2a6af3f64f154be02e8dbb6e2b27d94d9fe84a9
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-04 22:53:53 +0200

    Update WebkitConfiguration.java

--- src/webkit/main/java/org/comroid/webkit/config/WebkitConfiguration.java
+++ src/webkit/main/java/org/comroid/webkit/config/WebkitConfiguration.java
@@ -1,42 +1,44 @@
 package org.comroid.webkit.config;
 
 import org.comroid.api.ContextualProvider;
-import org.comroid.mutatio.model.Ref;
 import org.comroid.mutatio.ref.FutureReference;
-import org.comroid.uniform.adapter.xml.jsoup.JsoupXmlParser;
-import org.comroid.uniform.node.UniNode;
-import org.comroid.uniform.node.UniObjectNode;
-import org.comroid.varbind.annotation.RootBind;
-import org.comroid.varbind.bind.GroupBind;
-import org.comroid.varbind.bind.VarBind;
-import org.comroid.varbind.container.DataContainerBase;
 import org.comroid.webkit.frame.FrameBuilder;
-import org.jetbrains.annotations.ApiStatus.Internal;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
-import java.util.Collection;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@SuppressWarnings("ClassInitializerMayBeStatic")
-public final class WebkitConfiguration extends DataContainerBase<WebkitConfiguration> {
+public final class WebkitConfiguration implements ContextualProvider.Underlying {
     private static final FutureReference<WebkitConfiguration> instance = new FutureReference<>();
-    @Internal
-    @RootBind
-    public static GroupBind<WebkitConfiguration> Type;
-    private static VarBind<WebkitConfiguration, UniObjectNode, UniObjectNode, UniObjectNode> PARTS;
-    private static VarBind<WebkitConfiguration, UniObjectNode, UniObjectNode, UniObjectNode> PANELS;
-    private final Ref<UniObjectNode> parts;
-    private final Ref<UniObjectNode> panels;
+    private final ContextualProvider context;
+    private final Map<String, String> parts = new ConcurrentHashMap<>();
+    private final Map<String, String> panels = new ConcurrentHashMap<>();
 
-    private WebkitConfiguration(ContextualProvider context, UniObjectNode data) {
-        super(context, data);
-        this.parts = getComputedReference(PARTS);
-        this.panels = getComputedReference(PANELS);
+    @Override
+    public ContextualProvider getUnderlyingContextualProvider() {
+        return context;
+    }
+
+    private WebkitConfiguration(ContextualProvider context, Document dom) {
+        this.context = context.plus("Webkit Configuration", this);
+
+        dom.getElementsByTag("parts")
+                .first()
+                .children()
+                .forEach(element -> parts.put(element.tagName(), element.html()));
+        dom.getElementsByTag("panels")
+                .first()
+                .children()
+                .forEach(element -> panels.put(element.tagName(), element.html()));
     }
 
     public static WebkitConfiguration get() {
@@ -44,9 +46,8 @@ public final class WebkitConfiguration extends DataContainerBase<WebkitConfigura
     }
 
     public static WebkitConfiguration initialize(ContextualProvider context) {
-        if (instance.future.isDone() || Type != null)
+        if (instance.future.isDone())
             throw new IllegalStateException("Configuration is already initialized");
-        Type = new GroupBind<>(context, "webkit-configuration");
 
         InputStream config = FrameBuilder.getResource("config.xml");
         String data;
@@ -58,41 +59,32 @@ public final class WebkitConfiguration extends DataContainerBase<WebkitConfigura
         } catch (IOException e) {
             throw new RuntimeException("Could not find config.xml resource (" + FrameBuilder.RESOURCE_PREFIX + "config.xml)", e);
         }
-        if (Type == null)
-            throw new IllegalStateException("Initialization Process incorrect");
-        PARTS = Type.createBind("parts")
-                .extractAsObject()
-                .build();
-        PANELS = Type.createBind("panels")
-                .extractAsObject()
-                .build();
-        UniObjectNode initialData = JsoupXmlParser.instance.parse(data);
-        if (!instance.future.complete(new WebkitConfiguration(context, initialData)))
+        Document dom = Jsoup.parse(data, "", Parser.xmlParser());
+        if (!instance.future.complete(new WebkitConfiguration(context, dom)))
             throw new RuntimeException("Could not initialize Webkit Configuration");
+
         return instance.assertion("Initialization failed");
     }
 
     public Stream<String> streamPartNames() {
-        return parts.map(obj -> obj.keySet().stream())
-                .orElseGet(Stream::empty);
+        return parts.keySet().stream();
     }
 
     public InputStream getPart(String name) {
-        return parts.map(obj -> obj.get(name))
-                .map(UniNode::asString)
-                .map(resource -> FrameBuilder.getResource("part/" + resource))
-                .assertion("Could not find any part named " + name);
+        if (!parts.containsKey(name))
+            throw new NoSuchElementException("Could not find part with name " + name);
+        String resource = parts.get(name);
+        return FrameBuilder.getResource("part/" + resource);
     }
 
     public Stream<String> streamPanelNames() {
-        return panels.map(obj -> obj.keySet().stream())
-                .orElseGet(Stream::empty);
+        return panels.keySet().stream();
     }
 
     public InputStream getPanel(String name) {
-        return panels.map(obj -> obj.get(name))
-                .map(UniNode::asString)
-                .map(resource -> FrameBuilder.getResource("panel/" + resource))
-                .assertion("Could not find any panel named " + name);
+        if (!panels.containsKey(name))
+            throw new NoSuchElementException("Could not find panel with name " + name);
+        String resource = panels.get(name);
+        return FrameBuilder.getResource("panel/" + resource);
     }
 }
