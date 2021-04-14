d7d4195f6df7e30b772178b7c61651a5b7ad1343
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-14 14:38:36 +0200

    Update WebkitServer.java

--- src/webkit/main/java/org/comroid/webkit/server/WebkitServer.java
+++ src/webkit/main/java/org/comroid/webkit/server/WebkitServer.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledExecutorService;
