c8cea64955df11b59a3bd5d4d89791b58267cbfe
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-14 14:18:50 +0200

    class cleanup

--- src/webkit/main/java/org/comroid/webkit/server/WebSocketServer.java
+++ src/webkit/main/java/org/comroid/webkit/server/WebSocketServer.java
@@ -3,35 +3,50 @@ package org.comroid.webkit.server;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.comroid.api.ContextualProvider;
+import org.comroid.api.NFunction;
 import org.comroid.restless.REST;
 import org.comroid.restless.socket.WebsocketPacket;
 import org.comroid.webkit.socket.ConnectionFactory;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
+import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.function.BiFunction;
+import java.util.concurrent.Executor;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
 
-public class WebSocketServer extends org.java_websocket.server.WebSocketServer implements Closeable {
+public final class WebSocketServer extends org.java_websocket.server.WebSocketServer implements Closeable, ContextualProvider.Underlying {
     private static final Logger logger = LogManager.getLogger();
     private final ContextualProvider context;
-    private final ScheduledExecutorService executor;
+    private final Executor executor;
     private final String baseUrl;
-    private final BiFunction<WebSocket, REST.Header.List, ? extends WebSocketConnection> connectionFactory;
+    private final ConnectionFactory<? extends WebSocketConnection> connectionFactory;
     private final Map<WebSocket, WebSocketConnection> activeConnections;
+    private final Set<Consumer<WebSocketConnection>> connectionListeners;
+
+    @Override
+    public ContextualProvider getUnderlyingContextualProvider() {
+        return context;
+    }
+
+    public Executor getExecutor() {
+        return executor;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
 
     public WebSocketServer(
             ContextualProvider context,
-            ScheduledExecutorService executor,
+            Executor executor,
             String baseUrl,
             InetAddress inetAddress,
             int port
@@ -41,11 +56,22 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
 
     public <C extends WebSocketConnection> WebSocketServer(
             ContextualProvider context,
-            ScheduledExecutorService executor,
+            Executor executor,
             String baseUrl,
             InetAddress inetAddress,
             int port,
-            BiFunction<WebSocket, REST.Header.List, C> connectionFactory
+            NFunction.In3<WebSocket, REST.Header.List, Executor, ? extends WebSocketConnection> connectionConstructor
+    ) throws IOException {
+        this(context, executor, baseUrl, inetAddress, port, new ConnectionFactory<>(connectionConstructor, executor));
+    }
+
+    public <C extends WebSocketConnection> WebSocketServer(
+            ContextualProvider context,
+            Executor executor,
+            String baseUrl,
+            InetAddress inetAddress,
+            int port,
+            ConnectionFactory<C> connectionFactory
     ) throws IOException {
         super(new InetSocketAddress(inetAddress, port));
 
@@ -54,8 +80,15 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
         this.baseUrl = baseUrl;
         this.connectionFactory = connectionFactory;
         this.activeConnections = new ConcurrentHashMap<>();
+        this.connectionListeners = new HashSet<>();
 
         super.start();
+    }
+
+    public BooleanSupplier onNewConnection(Consumer<WebSocketConnection> listener) {
+        if (!connectionListeners.add(listener))
+            throw new RuntimeException("Could not add Connection listener");
+        return () -> connectionListeners.remove(listener);
     }
 
     @Override
@@ -73,6 +106,7 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
     }
 
     @Override
+    @Internal
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         Iterator<String> headerIter = handshake.iterateHttpFields();
         REST.Header.List headers = new REST.Header.List();
@@ -87,9 +121,11 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
 
         WebSocketConnection connection = connectionFactory.apply(conn, headers);
         activeConnections.put(conn, connection);
+        publishConnection(connection);
     }
 
     @Override
+    @Internal
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
         WebSocketConnection connection = findConnection(conn);
 
@@ -99,6 +135,7 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
     }
 
     @Override
+    @Internal
     public void onMessage(WebSocket conn, String message) {
         WebSocketConnection connection = findConnection(conn);
 
@@ -106,6 +143,7 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
     }
 
     @Override
+    @Internal
     public void onError(WebSocket conn, Exception ex) {
         WebSocketConnection connection = findConnection(conn);
 
@@ -113,6 +151,7 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
     }
 
     @Override
+    @Internal
     public void onStart() {
         logger.debug("Underlying WebSocketServer started");
     }
@@ -122,5 +161,10 @@ public class WebSocketServer extends org.java_websocket.server.WebSocketServer i
         if (found == null)
             throw new NoSuchElementException("No active connection available for " + conn);
         return found;
+    }
+
+    private void publishConnection(WebSocketConnection connection) {
+        for (Consumer<WebSocketConnection> listener : connectionListeners)
+            listener.accept(connection);
     }
 }
