86bb0386a6175b54aed99f2a64e7eefaf516de39
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-21 18:48:34 +0200

    MimeType improvements

--- src/restless/main/java/org/comroid/restless/MimeType.java
+++ src/restless/main/java/org/comroid/restless/MimeType.java
@@ -5,13 +5,14 @@ import org.comroid.api.Named;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public final class MimeType implements Named {
+public final class MimeType implements Named, CharSequence {
     private static final Map<String, MimeType> cache = new ConcurrentHashMap<>();
     public static final MimeType JSON = forName("application/json");
     public static final MimeType XML = forName("application/xml");
     public static final MimeType JAVASCRIPT = forName("application/javascript");
     public static final MimeType HTML = forName("text/html");
     private final String name;
+    private final int separator;
 
     @Override
     public String getName() {
@@ -20,23 +21,39 @@ public final class MimeType implements Named {
 
     @Override
     public String getAlternateFormattedName() {
-        return getSuffix();
+        return getType();
     }
 
     public String getPrefix() {
-        return name.split("/")[0];
+        return name.substring(0, separator);
     }
 
-    public String getSuffix() {
-        return name.split("/")[1];
+    public String getType() {
+        return name.substring(separator + 1);
     }
 
     private MimeType(String name) {
         this.name = name;
+        this.separator = name.indexOf('/');
     }
 
     public static MimeType forName(String name) {
         return cache.computeIfAbsent(name, MimeType::new);
+    }
+
+    @Override
+    public int length() {
+        return name.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return name.charAt(index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return name.subSequence(start, end);
     }
 
     @Override
