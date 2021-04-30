1f5c0ca497866a8f8c068125ae14584f91a3ceb0
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-04-30 21:59:38 +0200

    change findAccessToken method

--- src/webkit/oauth/java/org/comroid/webkit/oauth/client/ClientProvider.java
+++ src/webkit/oauth/java/org/comroid/webkit/oauth/client/ClientProvider.java
@@ -1,6 +1,7 @@
 package org.comroid.webkit.oauth.client;
 
 import org.comroid.api.Rewrapper;
+import org.comroid.restless.CommonHeaderNames;
 import org.comroid.webkit.oauth.user.OAuthAuthorization;
 import org.comroid.restless.REST;
 import org.comroid.restless.server.RestEndpointException;
@@ -10,7 +11,11 @@ import java.util.UUID;
 public interface ClientProvider {
     OAuthAuthorization findAuthorization(String authorizationCode) throws RestEndpointException;
 
-    OAuthAuthorization.AccessToken findAccessToken(REST.Header.List headers) throws RestEndpointException;
+    default OAuthAuthorization.AccessToken findAccessToken(REST.Header.List headers) throws RestEndpointException {
+        return findAccessToken(headers.getFirst(CommonHeaderNames.AUTHORIZATION));
+    }
+
+    OAuthAuthorization.AccessToken findAccessToken(String accessToken) throws RestEndpointException;
 
     boolean hasClient(UUID uuid);
 
