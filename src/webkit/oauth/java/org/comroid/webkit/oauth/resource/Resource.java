fa89416d96e39325177be60b09f8830b50c68e8c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-05-01 16:45:46 +0200

    Oauth service secrets (#8)
    * add getSecret method
    
    * validate resource secret
    
    * null check
    
    * fuck off

--- src/webkit/oauth/java/org/comroid/webkit/oauth/resource/Resource.java
+++ src/webkit/oauth/java/org/comroid/webkit/oauth/resource/Resource.java
@@ -2,8 +2,25 @@ package org.comroid.webkit.oauth.resource;
 
 import org.comroid.api.Named;
 import org.comroid.api.UUIDContainer;
+import org.comroid.util.Base64;
 import org.comroid.webkit.oauth.user.OAuthAuthorization;
 
 public interface Resource extends UUIDContainer, Named {
+    String getSecret();
+
+    default boolean checkBasicToken(String basicToken) {
+        if (basicToken == null)
+            return false;
+        basicToken = Base64.decode(basicToken);
+        if (!basicToken.startsWith("Basic "))
+            return false;
+        // strip prefix
+        int index = basicToken.indexOf(' ');
+        if (index < 8)
+            basicToken = basicToken.substring(index + 1);
+        String[] split = basicToken.split(":");
+        return split[0].equals(getUUID().toString()) && split[1].equals(getSecret());
+    }
+
     String generateAccessToken(OAuthAuthorization authorization);
 }
