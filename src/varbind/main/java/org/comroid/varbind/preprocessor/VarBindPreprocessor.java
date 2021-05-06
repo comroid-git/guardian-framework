fc7ec9096d59d1b53313ad9db64bd100f0a69c29
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-05-06 14:54:44 +0200

    Update VarBindPreprocessor.java

--- src/varbind/main/java/org/comroid/varbind/preprocessor/VarBindPreprocessor.java
+++ src/varbind/main/java/org/comroid/varbind/preprocessor/VarBindPreprocessor.java
@@ -5,8 +5,13 @@ import org.comroid.annotations.AnnotationProcessor;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
 
 @SupportedAnnotationTypes("org.comroid.varbind.annotation.RootBind")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public final class VarBindPreprocessor extends AnnotationProcessor {
+    @Override
+    public void onAnnotation(TypeElement annotation, Element annotated) {
+    }
 }
