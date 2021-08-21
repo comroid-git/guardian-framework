561da05284d211a5be4a7f2936e56e4f6e8daf3f
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-08-21 13:41:27 +0200

    add sql updateFrom and updateInto methods

--- src/varbind/main/java/org/comroid/varbind/DataContainerCache.java
+++ src/varbind/main/java/org/comroid/varbind/DataContainerCache.java
@@ -4,27 +4,132 @@ import org.comroid.api.ContextualProvider;
 import org.comroid.api.Polyfill;
 import org.comroid.mutatio.ref.KeyedReference;
 import org.comroid.mutatio.ref.Reference;
+import org.comroid.mutatio.ref.ReferenceList;
+import org.comroid.mutatio.span.Span;
 import org.comroid.uniform.cache.BasicCache;
 import org.comroid.uniform.cache.Cache;
 import org.comroid.uniform.node.UniObjectNode;
 import org.comroid.varbind.bind.VarBind;
 import org.comroid.varbind.container.DataContainer;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public class DataContainerCache<K, V extends DataContainer<? super V>>
         extends BasicCache<K, V>
         implements Cache<K, V> {
-    protected final VarBind<? super V, ?, ?, K> idBind;
+    protected final VarBind<? super V, K, ?, K> idBind;
+    protected final String idColumn;
 
     public DataContainerCache(
             ContextualProvider context,
             int largeThreshold,
-            VarBind<? super V, ?, ?, K> idBind
+            VarBind<? super V, K, ?, K> idBind
+    ) {
+        this(context, largeThreshold, idBind, null);
+    }
+
+    public DataContainerCache(
+            ContextualProvider context,
+            int largeThreshold,
+            VarBind<? super V, K, ?, K> idBind,
+            String idColumn
     ) {
         super(context, largeThreshold);
 
         this.idBind = Polyfill.uncheckedCast(idBind);
+        this.idColumn = idColumn;
+    }
+
+    public int updateFrom(ResultSet results) throws SQLException {
+        return updateFrom(results, idColumn);
+    }
+
+    public int updateFrom(final ResultSet results, String idColumn) throws SQLException {
+        int c = 0;
+        idColumn = Polyfill.notnullOr(idColumn, this.idColumn);
+
+        while (results.next()) {
+            final K id = results.getObject(idColumn, idBind.getHeldType().getTargetClass());
+            V container;
+            if (!containsKey(id) || (container = get(id)) == null) {
+                getLogger().debug("Skipped updating data for ID {} because the corresponding object was not found in cache {}", id, this);
+                continue;
+            }
+
+            //noinspection unchecked
+            for (VarBind<?, Object, ?, Object> bind : container.keySet()) {
+                final String updateKey = bind.getFieldName();
+                //noinspection rawtypes
+                KeyedReference<String, ReferenceList> ref = container.getInputReference(updateKey, true);
+
+                if (bind.isListing()) {
+                    // expect array
+                    // fixme WILL fail with primitive types
+                    final Object[] array = (Object[]) results.getArray(updateKey).getArray();
+                    ref.compute(refs -> {
+                        if (refs == null)
+                            return Span.immutable(array);
+                        refs.clear();
+                        //noinspection unchecked
+                        refs.addAll(Arrays.asList(array));
+                        return refs;
+                    });
+                    c++;
+                } else {
+                    // expect object
+                    final Object object = results.getObject(updateKey, bind.getHeldType().getTargetClass());
+                    ref.compute(refs -> {
+                        if (refs == null)
+                            return Span.immutable(object);
+                        refs.clear();
+                        //noinspection unchecked
+                        refs.add(object);
+                        return refs;
+                    });
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+
+    public int updateInto(ResultSet results) throws SQLException {
+        return updateInto(results, idColumn);
+    }
+
+    public int updateInto(final ResultSet results, String idColumn) throws SQLException {
+        int c = 0;
+        idColumn = Polyfill.notnullOr(idColumn, this.idColumn);
+
+        while (results.next()) {
+            final K id = results.getObject(idColumn, idBind.getHeldType().getTargetClass());
+            V container;
+            if (!containsKey(id) || (container = get(id)) == null) {
+                getLogger().debug("Skipped updating data for ID {} because the corresponding object was not found in cache {}", id, this);
+                continue;
+            }
+
+            //noinspection unchecked
+            for (VarBind<?, Object, ?, Object> bind : container.keySet()) {
+                final String sendKey = bind.getFieldName();
+                //noinspection rawtypes
+                KeyedReference<String, ReferenceList> ref = container.getInputReference(sendKey, true);
+
+                if (bind.isListing()) {
+                    // expect array
+                    throw new UnsupportedOperationException("UpdateInto cannot work with Arrays currently");
+                } else {
+                    // expect object
+                    final Object object = ref.ifPresentMap(refs -> refs.get(0));
+                    results.updateObject(sendKey, object);
+                    c++;
+                }
+            }
+        }
+        return c;
     }
 
     public boolean add(V value) {
